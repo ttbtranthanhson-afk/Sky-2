c53feb67255bbfb21a1114734abc30ddf2179ae1
ttbtranthanhson-afk <ttbtranthanhson@gmail.com>
2026-01-22 08:53:17 +0700

    Add files via upload
    Signed-off-by: ttbtranthanhson-afk <ttbtranthanhson@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll", policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowAll"); 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
